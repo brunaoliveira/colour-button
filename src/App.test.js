40fe0e52e089ce066debcddaf8377b1f1a51cd26
Bruna Oliveira <Bruna_Oliveira1@Dellteam.com>
2021-11-22 15:21:04 -0300

    Test when button clicked

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,18 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import App from "./App";
 
 test("button has correct initial colour", () => {
   render(<App />);
   const buttonElement = screen.getByRole("button", { name: "Change to blue" });
 
-  expect(buttonElement).toBe;
-  expect(buttonElement).toHaveStyle({backgroundColor: 'red'});
+  expect(buttonElement).toHaveStyle({ backgroundColor: "red" });
 });
 
-test("button turns blue when clicked", () => {});
+test("button turns blue when clicked", () => {
+  render(<App />);
+  const buttonElement = screen.getByRole("button", { name: "Change to blue" });
+  fireEvent.click(buttonElement);
+
+  expect(buttonElement).toHaveStyle({ backgroundColor: "blue" });
+  expect(buttonElement).toHaveTextContent("Change to red");
+});
